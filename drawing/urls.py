eed4cac8774350040d3bbccdef0f651300dd8b13
David Pollard <pollardld@gmail.com>
2017-05-10 12:31:16 -0700

    rpc4django urls added

--- drawing/urls.py
+++ drawing/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from .views import *
+from rpc4django.views import serve_rpc_request
 
 urlpatterns = [
     #drawings
@@ -7,4 +8,5 @@ urlpatterns = [
     #feature reports
     url(r'^wind_report/(\d+)', wind_analysis, name='wind_analysis'), #user requested wind energy site analysis
     url(r'^aoi_report/(\d+)', aoi_analysis, name='aoi_analysis'), #user requested area of interest analysis
+    url(r'^rpc$', serve_rpc_request, name='rpc'), #user requested area of interest analysis
 ]
