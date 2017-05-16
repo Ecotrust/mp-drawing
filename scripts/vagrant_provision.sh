d76351b765acd610360f044639ae6d708adc0f9d
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-05-16 13:50:50 -0700

    updating deployment on marineplanner-core

---
+++ scripts/vagrant_provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PROJECT_NAME=$1
+MODULE_NAME=mp-drawing
+
+PROJECT_DIR=/usr/local/apps/$PROJECT_NAME
+VIRTUALENV_DIR=$PROJECT_DIR/env
+MODULE_DIR=$PROJECT_DIR/apps/$MODULE_NAME
+
+PYTHON=$VIRTUALENV_DIR/bin/python
+PIP=$VIRTUALENV_DIR/bin/pip
+
+$PIP install -r $MODULE_DIR/requirements.txt
+
+$PYTHON $PROJECT_DIR/marineplanner/manage.py enable_sharing --all
