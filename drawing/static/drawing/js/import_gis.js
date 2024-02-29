26a37ebb99767ebc58109b348a4c1d543d500438
Ryan Hodges <rhodges@users.noreply.github.com>
2024-02-29 10:01:08 -0800

    updated language to call out KML support

--- drawing/static/drawing/js/import_gis.js
+++ drawing/static/drawing/js/import_gis.js
@@ -1,7 +1,7 @@
 $('#gisfile').on('change', function(){
     let validity = validate_input_file()
     if (validity.length > 0 && !validity.type_valid) {
-        window.alert('File type not recognized. Is your file a ".shp", ".zip", ".csv", ".json", or ".geojson"?');
+        window.alert('File type not recognized. Is your file a ".shp", ".zip", ".csv", ".kml", ".kmz", ".json", or ".geojson"?');
         $('#gisfile').val('');
     } else if (validity.is_valid && validity.type_valid) {
         interpret_file();
